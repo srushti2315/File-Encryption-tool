79a086f560dafcf471a8b0819bd64f051818cfe4
om <ombhutkar9@gmail.com>
2024-04-05 12:52:28 +0530

    3rd

--- inbox.py
+++ inbox.py
@@ -1,24 +1,40 @@
-import customtkinter
+import customtkinter 
 from customtkinter import *
 from PIL import Image
 from tkinter import filedialog, messagebox
 import sqlite3
 class InboxApp:
     def __init__(self, userinf):
-        self.userinf = userinf
-        self.root = CTk()
-        self.root.title("Inbox")
-        
+        self.main = CTk()
+        self.main.title("Home Page")
+        self.main.config(bg="white")
+        self.userinf=userinf
         self.conn = sqlite3.connect("loginDB.db")
         self.cursor = self.conn.cursor()
 
-        self.inbox_frame = CTkFrame(self.root)
-        self.inbox_frame.pack()
+        
+        screen_width = self.main.winfo_screenwidth()
+        screen_height = self.main.winfo_screenheight()
 
+        self.main.geometry(f"{screen_width}x{screen_height}")
+
+        self.center_window()
+
+        self.navbar = CTkFrame(self.main, height=50, fg_color="#121212",width=1600,border_width=2,border_color="purple")
+        self.navbar.grid(row=0,columnspan=10 ,sticky="ew")
+
+        self.home_btn = CTkButton(self.navbar, text="Home", fg_color="black",height=40,border_width=2,border_color="#39FF14",font=("", 15, "bold"),hover_color="green")
+        self.home_btn.place(relx=0.5, rely=0.5, anchor="center")
+        self.about_btn = CTkButton(self.navbar, text="About", fg_color="black",height=40,border_width=2,border_color="#39FF14",font=("", 15, "bold"),hover_color="green")
+        self.about_btn.place(relx=0.4, rely=0.5, anchor="center")
+        self.contact_btn = CTkButton(self.navbar, text="Contact Us", fg_color="black",height=40,border_width=2,border_color="#39FF14",font=("", 15, "bold"),hover_color="green")
+        self.contact_btn.place(relx=0.6, rely=0.5, anchor="center")
+        self.inbox_frame = CTkFrame(self.main,height=600,width=200,fg_color="blue")
+        self.inbox_frame.grid(row=2, column=0, sticky="w")
+        self.main.grid_columnconfigure(0, weight=1)
 
         self.display_inbox()
-
-        self.root.mainloop()
+        self.main.mainloop()
 
     def display_inbox(self):
         self.cursor.execute("SELECT senders_name, subject, message, attachment_name  FROM {}".format(self.userinf))
@@ -26,12 +42,21 @@ class InboxApp:
 
         for index, message in enumerate(messages, start=1):
             sender, subject, mes, attachment_name  = message
-            message_label = CTkLabel(self.inbox_frame, text=f"{index}. From: {sender}, Subject: {subject}")
-            message_label.pack()
+            message_label = CTkLabel(self.inbox_frame, text=f"{index}. From: {sender}, Subject: {subject}",height=50,width=300,fg_color="black")
+            message_label.grid(row=index,column=0)
+            self.inbox_frame.grid(row=1, column=0, sticky="w", columnspan=10)
+            self.main.grid_rowconfigure(2, weight=1)
+
         
+
+
+
         
-    def __del__(self):
-        self.conn.close()
+
+    def center_window(self):
+        x = (self.main.winfo_reqwidth() - 200) 
+        y = (self.main.winfo_reqheight() - 200) 
+        self.main.geometry(f"+{x}+{y}")
 
 if __name__ == "__main__":
     userinf = "om" 
